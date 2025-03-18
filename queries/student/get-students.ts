865cb5d7cc0765a2a5f1284ad2ceabea7d015361
Henefisa <jickyetran@outlook.com>
2025-03-18 15:17:34 +0000

    feat: add custom hooks for authentication and debouncing; enhance student query with status filtering

--- queries/student/get-students.ts
+++ queries/student/get-students.ts
@@ -1,5 +1,37 @@
 import { TypedSupabaseClient } from '@/utils/types';
 
-export const getStudents = (client: TypedSupabaseClient) => {
-  return client.from('students').select(`*, profiles(*)`).throwOnError();
+export enum StudentStatusFilter {
+  ALL = 'all',
+  ACTIVE = 'active',
+  INACTIVE = 'inactive',
+}
+
+export const getStudents = (
+  client: TypedSupabaseClient,
+  options: {
+    status: StudentStatusFilter;
+    q: string;
+  }
+) => {
+  const query = client
+    .from('students')
+    .select(`*, profiles!inner (*)`)
+    .throwOnError();
+
+  if (options.status === StudentStatusFilter.ACTIVE) {
+    query.eq('status', true);
+  } else if (options.status === StudentStatusFilter.INACTIVE) {
+    query.eq('status', false);
+  }
+
+  if (options.q) {
+    query.or(
+      `first_name.ilike.%${options.q}%, last_name.ilike.%${options.q}%`,
+      {
+        referencedTable: 'profiles',
+      }
+    );
+  }
+
+  return query;
 };
